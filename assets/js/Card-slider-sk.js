2ed475ee20ec27fc3c7b3df674b9c1ecb74c41d7
gokul1099 <sgokulcse10@gmail.com>
2020-05-17 21:06:30 +0530

    added html and css

---
+++ assets/js/Card-slider-sk.js
@@ -0,0 +1,6 @@
+(function($){
+	   "use strict";
+	   $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+	   $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+   })	
+   (jQuery);
